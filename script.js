c46925fb7767ca9a4e193db054a610c9ed0727ab
HADRIAN_JAVAS <Hadrianjavas12@gmail.com>
2023-11-13 09:09:31 +0700

    per ruangan

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+const lampuOn = "assets/images/on.gif";
+const lampuOff = "assets/images/off.gif";
+
 function saklarToggle(idToggle, idlampu) {
     let lampu = document.getElementById(idlampu);
     let toggle = document.getElementById(idToggle);
+    onOffLampu(idlampu,toggle.checked);
+}
 
-    if (toggle.checked) {
-        lampu.src = "assets/images/on.gif";
+function checkboxClick(checkBoxId, ruangan){
+    var lampuRuangTamu = document.getElementsByClassName(ruangan);
+    let checkBox = document.getElementById(checkBoxId);
+    for(var i = 0; i < lampuRuangTamu.length; i++) {
+        onOffLampu(lampuRuangTamu[i].id,checkBox.checked);
+    }
+}
+
+function onOffLampu(idlampu, condition){
+    let lampu = document.getElementById(idlampu);
+    if (condition) {
+        lampu.src = lampuOn;
     }
     else {
-        lampu.src = "assets/images/off.gif";
+        lampu.src = lampuOff;
     }
-
 }
